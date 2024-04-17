9b8311cb4470e736a8f8e416fc2b61f25ec1278c
winscripter <142818255+winscripter@users.noreply.github.com>
2024-04-17 12:51:16 +0400

    Upload Exceptions folder

---
+++ dotnetVEE/Abstractions/Exceptions/UnsupportedVideoFormatException.cs
@@ -0,0 +1,42 @@
+﻿namespace dotnetVEE.Abstractions.Exceptions
+{
+    /// <summary>
+    /// An error, representing an unsupported video format.
+    /// </summary>
+    public class UnsupportedVideoFormatException : Exception
+    {
+        /// <summary>
+        /// Name of the unsupported video format.
+        /// </summary>
+        public string? FormatName { get; init; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnsupportedVideoFormatException" /> class.
+        /// </summary>
+        public UnsupportedVideoFormatException()
+            : this("The video format is not supported by dotnetVEE.")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnsupportedVideoFormatException" /> class.
+        /// </summary>
+        /// <param name="message">Exception message.</param>
+        public UnsupportedVideoFormatException(string message)
+            : base(message)
+        {
+            FormatName = null;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnsupportedVideoFormatException" /> class.
+        /// </summary>
+        /// <param name="message">Exception message.</param>
+        /// <param name="formatName">Format name.</param>
+        public UnsupportedVideoFormatException(string message, string formatName)
+            : base($"{message}{Environment.NewLine}Format name: \"{formatName}\"")
+        {
+            FormatName = formatName;
+        }
+    }
+}
