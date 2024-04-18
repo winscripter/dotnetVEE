8ea0e4f96033d57c4be0c6f7226c70e3374ae5fd
winscripter <142818255+winscripter@users.noreply.github.com>
2024-04-18 12:30:28 +0400

    Use expression bodied member and discard assignment

--- dotnetVEE/Computation/PrivateVideoExtensions.cs
+++ dotnetVEE/Computation/PrivateVideoExtensions.cs
@@ -5,8 +5,6 @@ namespace dotnetVEE.Computation
     internal static class PrivateVideoExtensions
     {
         public static void ExtractFrameAt(this Video video, int frameIndex, string @as)
-        {
-            string output = InvokeHelper.LaunchAndRedirectFFmpegOutput($"-i \"{video.Path}\" -vf \"select=eq(n\\,{frameIndex})\" -vframes 1 \"{@as}\"");
-        }
+            => _ = InvokeHelper.LaunchAndRedirectFFmpegOutput($"-i \"{video.Path}\" -vf \"select=eq(n\\,{frameIndex})\" -vframes 1 \"{@as}\"");
     }
 }
